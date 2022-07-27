d9de58b9b23da682c550f51dde0db0ebf480768c
Mikail Khan <mikail@mikail-khan.com>
2022-07-27 19:03:14 -0400

    cleaned up imports

--- laminar/src/main/scala/doodle/explore/Main.scala
+++ laminar/src/main/scala/doodle/explore/Main.scala
@@ -1,44 +1,21 @@
 package doodle.explore.laminar
 
-import cats.effect.{IO, IOApp}
-import cats.effect.IO.asyncForIO
-import cats.effect.IO
-
-import scala.concurrent.duration.DurationInt
-
-// import doodle.svg.effect.Center._
-// import doodle.svg.effect.Redraw._
-// import doodle.svg.effect.Size._
-
 import doodle.core._
 import doodle.image._
-import doodle.image.syntax._
-import doodle.image.syntax.all._
 import doodle.image.syntax.core._
 
-import com.raquo.laminar.api.L.{*, given}
-import org.scalajs.dom
-
-import doodle.explore.{ExploreInt, ExploreChoice, ExploreButton}
+import doodle.explore.{ExploreInt, ExploreChoice, ExploreButton, ExploreColor, Layout}
 import doodle.explore.IntComponentOps._
-import doodle.syntax.all.{RendererFrameOps, AngleIntOps}
-import doodle.explore.laminar.Component
+import doodle.explore.LayoutOps._
 
-import doodle.interact.effect.AnimationRenderer
-import doodle.effect.Renderer
 import doodle.svg.effect.Frame
-import doodle.svg.effect.Size.FixedSize
+import doodle.svg.svgAnimationRenderer
+import doodle.svg.svgRenderer
 
 import org.scalajs.dom
 import org.scalajs.dom.document
-import cats.effect.unsafe.implicits.global
 
-import doodle.svg.svgAnimationRenderer
-import doodle.svg.svgRenderer
-import doodle.explore.Layout
-import doodle.explore.LayoutOps._
-import doodle.explore.ExploreColor
-
+import com.raquo.laminar.api.L.{*, given}
 import doodle.image.Image
 
 object Fractals {
