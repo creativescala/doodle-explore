87139dbeb774f6b1294b60c3c4e8c2b41d6607ad
Mikail Khan <mikail@mikail-khan.com>
2022-08-18 23:20:00 -0400

    Fixed Choice implementation to properly avoid extra match cases,
    - removed toChoices
    - removed asButton and asCheckbox, resolving #3

--- core/src/main/scala/doodle/explore/ExploreBoolean.scala
+++ core/src/main/scala/doodle/explore/ExploreBoolean.scala
@@ -17,11 +17,6 @@
 package doodle.explore
 
 trait ExploreBoolean[F[_]] {
-  def boolean(label: String): F[Boolean]
-
-  def asButton(generator: F[Boolean]): F[Boolean]
-  def asCheckbox(generator: F[Boolean]): F[Boolean]
-
-  def button(label: String) = asButton(boolean(label))
-  def checkbox(label: String) = asCheckbox(boolean(label))
+  def button(label: String): F[Boolean]
+  def checkbox(label: String): F[Boolean]
 }
