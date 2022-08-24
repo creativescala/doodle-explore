97cadcd1d899e763dea168c1c6a2fc67692797d6
Noel Welsh <noel@noelwelsh.com>
2022-08-24 12:05:15 +0100

    Jazz up the example
    Add some color for more visual interest and a more impressive demonstration

--- example/src/main/scala/example/Example.scala
+++ example/src/main/scala/example/Example.scala
@@ -3,15 +3,19 @@ package example
 import doodle.core.*
 import doodle.syntax.all.*
 import doodle.explore.*
-import doodle.explore.laminar.{Component, IntInterpreter}
+import doodle.explore.laminar.*
 import doodle.svg.*
 import scala.scalajs.js.annotation.*
 
 @JSExportTopLevel("Example")
 object Example {
-  def concentricCircles(count: Int): Picture[Unit] =
+  def concentricCircles(count: Int, color: Color): Picture[Unit] =
     if count == 0 then Picture.empty
-    else Picture.circle(count * 20).on(concentricCircles(count - 1))
+    else
+      Picture
+        .circle(count * 20)
+        .fillColor(color.spin(10.degrees * count))
+        .on(concentricCircles(count - 1, color))
 
   // Explore the concentricCircles example
   //
@@ -20,14 +24,16 @@ object Example {
   def go(mountId: String): Unit = {
     val frame = Frame(mountId)
     val initialCount = 4
+    val initialColor = Color.midnightBlue
 
     IntInterpreter
       .int("Count")
       .within(1, 20)
       .withDefault(initialCount)
+      .above(ColorInterpreter.color("Color"))
       .explore(
         frame,
-        count => concentricCircles(count)
+        (count, color) => concentricCircles(count, color)
       )
   }
 }
